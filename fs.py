b16f1e5a7a774ae24b1bca39647e7e5318528efe
esel7353 <esel@gegensinn.org>
2013-05-08 12:32:55 +0200

    added comments, deleted useless function E_anomal

--- fs.py
+++ fs.py
@@ -1,21 +1,24 @@
 import scipy.constants as sc
 from math import *
 
+
+# Energy of a state n in the bohr model without any additional term
 def E_n(n):
 	return - sc.m_e * sc.e**4/(8*sc.epsilon_0**2 * sc.h**2 * n**2)
 
+# Energy of a state n, j in the bohr model with relat., Darim and LS terms
 def E_nj(n, j):
 	return E_n(n) * (1- sc.alpha**2 / n * (1/(j+0.5) - 3/(4*n)))
 
+# Lande factor
 def g_j(l, s, j):
 	return 1 + (j*(j+1) + s*(s+1) - l*(l+1)) / (2 * j *(j+1))
 
-def E_anomal(n, l, s, j):
-	return (E_nj(n, j) + deltaE_anomal(l, s, j), E_nj(n, j) - deltaE_anomal(l, s, j))
-
+# Energy/B distance -- anomal Zeeman effect
 def deltaE_anomal(l, s, j):
 	return	 g_j(l, s, j) * bohrMag
 
+# combination of three values
 def g_dE_Ea(n, l, s, j):
 	return [g_j(l, s, j), deltaE_anomal(l, s, j) / sc.eV / sc.micro, E_nj(n, j) / sc.eV]
 
